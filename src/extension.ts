9392f3c992ed9cd54b45687b90e33006d9ae3a56
Dmitry Gerasimov <di.gerasimov@gmail.com>
2020-09-19 01:14:24 +0300

    Use newer showTextDocument API

--- src/extension.ts
+++ src/extension.ts
@@ -14,14 +14,17 @@ export function activate(context: ExtensionContext) {
     // register command that crafts an uri with the `disassembly` scheme,
     // open the dynamic document, and shows it in the next editor
     const commandRegistration = commands.registerTextEditorCommand('disasexpl.show', srcEditor => {
-        let asmUri = encodeAsmUri(srcEditor.document.uri);
+        const asmUri = encodeAsmUri(srcEditor.document.uri);
 
-        workspace.openTextDocument(asmUri).then(doc => {
-            window.showTextDocument(doc, srcEditor.viewColumn! + 1, true).then(asmEditor => {
-                const decorator = new AsmDecorator(srcEditor, asmEditor, provider);
-                // dirty way to get decorations work after showing disassembly
-                setTimeout(_ => decorator.updateSelection(srcEditor), 500);
-            });
+        const options = {
+            viewColumn: srcEditor.viewColumn! + 1,
+            preserveFocus: true,
+        };
+
+        window.showTextDocument(asmUri, options).then(asmEditor => {
+            const decorator = new AsmDecorator(srcEditor, asmEditor, provider);
+            // dirty way to get decorations work after showing disassembly
+            setTimeout(_ => decorator.updateSelection(srcEditor), 500);
         });
     });
 
